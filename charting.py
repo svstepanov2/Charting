72ebcb3d612a8666e1f8d9904fb6c86fd7ff50e6
svstepanov2 <153940801+svstepanov2@users.noreply.github.com>
2024-08-15 20:08:22 +0500

    Update charting.py

--- charting.py
+++ charting.py
@@ -1,5 +1,37 @@
 import pandas as pd
-values = ['robot', 'robot', 'robot', 'robot', 'robot', 'human', 'human', 'human', 'human', 'human']
-one_hot = pd.DataFrame(index=values, columns=['robot', 'human'])
-one_hot.replace({'robot': 1, 'human': 0}, inplace=True)
+
+def one_hot_encode(df, column):
+    """
+    Функция преобразует столбец в one hot вид.
+
+    Args:
+        df (DataFrame): DataFrame для преобразования
+        column (str): имя столбца, который нужно преобразовать
+
+    Returns:
+        DataFrame: DataFrame с преобразованным столбцом в one hot вид
+    """
+
+    # Создаем пустой DataFrame с индексами исходного столбца
+    one_hot = pd.DataFrame(index=df[column].unique(), columns=df[column].unique())
+
+    # Заполняем DataFrame нулями
+    one_hot.fillna(0, inplace=True)
+
+    # Подставляем значения из исходного DataFrame
+    one_hot.iloc[df.index, df[column]] = 1
+
+    # Возвращаем DataFrame
+    return one_hot
+
+# Генерируем случайный DataFrame
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+
+# Преобразуем столбец в one hot вид
+one_hot = one_hot_encode(data, 'whoAmI')
+
+# Выводим результат
 print(one_hot)
